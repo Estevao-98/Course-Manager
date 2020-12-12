013679ff09d621bf6f1303a7003c6bb94bb99c6c
Estevao Manhaes <estevaoe298@gmail.com>
2020-12-12 00:31:55 -0300

    Modulos Pipe e Star

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
+import { StarComponent } from './shared/component/star/star.component';
+import { ReplacePipe } from './shared/pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 
