7b8e9cbc549300470dedee98b82b7b9f844e4738
Estevao Manhaes <estevaoe298@gmail.com>
2020-12-11 23:59:19 -0300

    Modulo Course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,29 +8,25 @@ import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+
 import { Error404Component } from './error-404/error-404.component';
 import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
-    CourseInfoComponent,
     Error404Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -19,9 +19,27 @@ export class CourseListComponent implements OnInit{
   }
 
   ngOnInit() : void {
-    this._courses = this.courseService.retrieveAll();
-    this.filteredCourses = this._courses;
+    this.retrieveAll();
 
+  }
+
+  retrieveAll(): void {
+    this.courseService.retrieveAll().subscribe({
+      next: courses => {
+        this._courses = courses;
+        this.filteredCourses = this._courses;
+      },
+      error: err => console.log('Error', err)
+    });
+  }
+
+  deleteById(courseId: number): void{
+    this.courseService.deleteById(courseId).subscribe({
+      next: () => {
+        console.log("Deleted with success");
+        this.retrieveAll();
+      }
+    });
   }
 
   set filter(value: string){
